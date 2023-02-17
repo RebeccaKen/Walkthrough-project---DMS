fd52f5334dffee4c71981a41873fb0a97322a2dd
RebeccaKen <kennedyrebecca694@gmail.com>
2023-02-17 15:54:32 +0000

    Save further tutor edits

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -5,22 +5,25 @@ if os.path.exists("env.py"):
     import env  # noqa
 from .database import db
 
+def create_app():
+    app = Flask('taskmanager')
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+    if os.environ.get("DEVELOPMENT") == "True":
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    else:
+        uri = os.environ.get("DB_URL")
+        if uri.startswith("postgres://"):
+            uri = uri.replace("postgres://", "postgresql://", 1)
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
-if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-else:
-    uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
-db.init_app(app)
+    return app
 
-with app.app_context():
-    db.create_all()
-    db.session.commit()
+
+app = create_app()
 
 from taskmanager import routes  # noqa
